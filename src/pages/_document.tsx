ec74ee1be66dd326959c077770987b0a9e8be639
christinae20492 <125934369+christinae20492@users.noreply.github.com>
2025-11-21 09:43:55 -0500

    favicon

---
+++ src/pages/_document.tsx
@@ -0,0 +1,28 @@
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel='icon' href='/favicon.ico' type='image/x-icon' />
+          <link rel="icon" href="/icon.png" type="image/png" />
+          <link rel="icon" href="/icon-01.svg" type="image/svg+xml" />
+          <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+          <link rel="manifest" href="/site.webmanifest" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
